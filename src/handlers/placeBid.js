908bcc71ae59a0fcfc4df1c5b0c07886004c46ee
Shiran Milinda <jmmilinda@wiley.com>
2022-05-19 11:55:44 +0530

    init: Adds initial project structer with basic functionality.

---
+++ src/handlers/placeBid.js
@@ -0,0 +1,45 @@
+import AWS from 'aws-sdk';
+import createError from 'http-errors';
+import commonMiddleware from '../libs/commonMiddleware';
+import {getAuctionById} from './getAuction';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+  let updatedAuction;
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+  const auction = await getAuctionById(id);
+
+  if(auction.status !== 'OPEN'){
+    throw new createError.Forbidden(`You can't bid ${id} auction.`);
+  }
+
+  if(amount <= auction.highestBid.amount){
+    throw new createError.Forbidden(`Your bid must be highest than ${auction.highestBid.amount}`);
+  }
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: {id},
+    UpdateExpression: 'set highestBid.amount = :amount',
+    ExpressionAttributeValues: {
+      ':amount': amount,
+    },
+    ReturnValues: 'ALL_NEW'
+  };
+
+  try {
+    const result = await dynamodb.update(params).promise();
+    updatedAuction = result.Attributes;
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(updatedAuction),
+  };
+}
+
+export const handler = commonMiddleware(placeBid);
---
+++ src/handlers/processAuctions.js
@@ -0,0 +1,18 @@
+import { getEndedAuctions } from '../libs/getEndedAuctions';
+import { closeAuction } from '../libs/closeAuction';
+import createError from 'http-errors';
+
+async function processAuctions(event, context) {
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map(auction => closeAuction(auction));
+    await Promise.all(closePromises);
+
+    return { closed: closePromises.length };
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
+}
+
+export const handler = processAuctions;
